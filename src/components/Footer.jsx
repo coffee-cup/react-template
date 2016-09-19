c22b4a3a48fcca72a9c51d9b985044bf7abeeca8
Jake Runzer <jakerunzer@gmail.com>
2016-09-19 01:17:34 -0700

    smaller footer text size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ class Footer extends Component {
             <footer className="gutter-small">
                 <div className="grid-centered-noGutter pv4 center-vertical">
                     <div className="col-9_xs-12 mb0">
-                        <p className="f4">
+                        <p className="f5">
                             Made with <span className="heart">&hearts;</span> by <a href='https://jakerunzer.xyz'>jake runzer</a>
                         </p>
                     </div>
